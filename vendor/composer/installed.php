b1540db64d3b807364f7e8071d98835c1d6673d6
Piotr Kaczmarek <piotr@prkropka.pl>
2023-11-17 16:52:14 +0100

    0.0.3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'piotr-kaczmarek/cbrnetheme',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9ce0795759e4968226ba2f61eb4be4d706cfd948',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'piotr-kaczmarek/cbrnetheme' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9ce0795759e4968226ba2f61eb4be4d706cfd948',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
